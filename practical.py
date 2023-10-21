21ee32fbfcfa23a07e52f5dec9ab59f15622e9fd
yyggs <67227210+yyggs@users.noreply.github.com>
2023-10-21 23:39:57 +0100

    Update practical.py

--- practical.py
+++ practical.py
@@ -1,53 +1,57 @@
 #!/usr/bin/env python
 
-import rdflib
-from rdflib.plugins.sparql import prepareQuery
+from rdflib import Graph, Namespace
 import pprint
 
 # 设定你的SPARQL端点
 SPARQL_ENDPOINT = 'http://localhost:3030/s2484724-epcc/sparql'
 
-g = rdflib.Graph()
-g.parse(SPARQL_ENDPOINT, format="xml")
+# 创建RDF图形实例
+g = Graph()
+
+# 使用RDFLib和SPARQL查询数据
+FOAF = Namespace("http://xmlns.com/foaf/0.1/")
+DBPEDIA = Namespace("http://dbpedia.org/resource/")
 
 # 查询并打印数据集中的所有三元组
-query_all_triples = prepareQuery("""
+all_triples_query = """
 SELECT ?s ?p ?o WHERE {
     ?s ?p ?o
 }
-""", initNs = {"rdf": rdflib.RDF.NAMESPACE})
+"""
+result = g.query(all_triples_query, initBindings={'sparql': SPARQL_ENDPOINT})
 
-for s, p, o in g.query(query_all_triples):
-    print(s, p, o)
+for row in result:
+    print(row)
 
 print()
 print('*********************')
 print()
 
 # 查询RDF数据中的人名
-query_names = prepareQuery("""
+q1 = """
 PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
 SELECT ?name WHERE {
     ?person foaf:name ?name
 }
-""", initNs = {"foaf": rdflib.Namespace("http://xmlns.com/foaf/0.1/")})
-
-for row in g.query(query_names):
-    print(row[0])
+"""
+result = g.query(q1, initBindings={'sparql': SPARQL_ENDPOINT})
+for row in result:
+    print(row)
 
 print()
 print('*********************')
 print()
 
 # 查询住在爱丁堡附近的人
-query_edinburgh = prepareQuery("""
+q2 = """
 PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
 PREFIX dbpedia: <http://dbpedia.org/resource/> 
 SELECT ?name WHERE {
     ?person foaf:based_near dbpedia:Edinburgh;
            foaf:name ?name
 }
-""", initNs = {"foaf": rdflib.Namespace("http://xmlns.com/foaf/0.1/")})
-
-for row in g.query(query_edinburgh):
-    print(row[0])
+"""
+result = g.query(q2, initBindings={'sparql': SPARQL_ENDPOINT})
+for row in result:
+    print(row)
