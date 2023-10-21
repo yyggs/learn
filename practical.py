6952136426c132fa3a4265ceac07bc2547442275
yyggs <67227210+yyggs@users.noreply.github.com>
2023-10-22 00:09:26 +0100

    Update practical.py

--- practical.py
+++ practical.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 #
 import rdflib
-from rdflib.plugins.sparql import prepareQuery
 import pprint
 
 # 从foaf_example.ttl文件中读取数据
 graph = rdflib.Graph()
 graph.parse("foaf_example.ttl", format="turtle")
 
-# 查询并打印数据集中的所有三元组
 sparql_query = """
 SELECT ?s ?p ?o WHERE {
     ?s ?p ?o
@@ -22,7 +20,6 @@ print()
 print('*********************')
 print()
 
-# 查询RDF数据中的人名
 sparql_query = """
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 SELECT ?name WHERE {
@@ -36,10 +33,9 @@ print()
 print('*********************')
 print()
 
-# 查询住在爱丁堡附近的人
 sparql_query = """
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX dbpedia: <http://dbpedia.org/resource/>
+PREFIX dbpedia: <http://dbpedia.org/resource/#>
 SELECT ?name WHERE {
     ?person foaf:based_near dbpedia:Edinburgh;
            foaf:name ?name
