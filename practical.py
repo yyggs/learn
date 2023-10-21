befbd91742b14d9af013e46a2319a610ff00590e
yyggs <67227210+yyggs@users.noreply.github.com>
2023-10-21 23:03:54 +0100

    Update practical.py

--- practical.py
+++ practical.py
@@ -1,46 +1,51 @@
-import rdflib
-from rdflib import Graph
-from rdflib.plugins.sparql import prepareQuery
+#!/usr/bin/env python
 
-# Initialize the RDF graph
+from rdflib import Graph, Namespace
+import pprint
+
+# 设定你的SPARQL端点
+SPARQL_ENDPOINT = 'http://localhost:3030/s2484724-epcc/sparql'
+
+# 创建RDF图形实例
 g = Graph()
 
-# Fetch and parse the RDF data from the SPARQL endpoint
+# 加载RDF数据
 g.parse(SPARQL_ENDPOINT, format="application/rdf+xml")
 
-# Query and print all the triples in the dataset
-query = prepareQuery("SELECT ?s ?p ?o WHERE {?s ?p ?o}")
-print(f'SPARQL query -> {query}')
-for row in g.query(query):
+# 使用RDFLib和SPARQL查询数据
+FOAF = Namespace("http://xmlns.com/foaf/0.1/")
+DBPEDIA = Namespace("http://dbpedia.org/resource/")
+
+# 查询并打印数据集中的所有三元组
+for s, p, o in g.triples((None, None, None)):
+    print(s, p, o)
+
+print()
+print('*********************')
+print()
+
+# 查询RDF数据中的人名
+q1 = """
+PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+SELECT ?name WHERE {
+    ?person foaf:name ?name
+}
+"""
+for row in g.query(q1):
     print(row)
 
 print()
 print('*********************')
 print()
 
-# Query RDF data for people's names
-query = prepareQuery(
-    '''
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
-    SELECT ?name WHERE {?person foaf:name ?name}
-    '''
-)
-print(f'SPARQL query -> {query}')
-for row in g.query(query):
-    print(row)
-
-print()
-print('*********************')
-print()
-
-# Query for people who live near Edinburgh
-query = prepareQuery(
-    '''
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
-    PREFIX dbpedia: <http://dbpedia.org/resource/#> 
-    SELECT ?name WHERE {?person foaf:based_near dbpedia:Edinburgh; foaf:name ?name}
-    '''
-)
-print(f'SPARQL query -> {query}')
-for row in g.query(query):
+# 查询住在爱丁堡附近的人
+q2 = """
+PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+PREFIX dbpedia: <http://dbpedia.org/resource/> 
+SELECT ?name WHERE {
+    ?person foaf:based_near dbpedia:Edinburgh;
+           foaf:name ?name
+}
+"""
+for row in g.query(q2):
     print(row)
